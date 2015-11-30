8a0d6da1106b2bd92cfcc27fe5dd2a536e943afd
Eduardofarinos <e.farinos.rijo@gmail.com>
2015-11-30 18:53:11 +0000

    rol

--- app.js
+++ app.js
@@ -1,72 +1,62 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// Express requires these dependencies
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
 
 var app = express();
 
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-server.listen(3001);
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+// Configure our application
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// Configure error handling
+app.configure('development', function(){
+  app.use(express.errorHandler());
 });
 
-// error handlers
+// Setup Routes
+app.get('/', routes.index);
+app.get('/users', user.list);
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+// Enable Socket.io
+var server = http.createServer(app).listen( app.get('port') );
+var io = require('socket.io').listen( server );
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+// A user connects to the server (opens a socket)
+io.sockets.on('connection', function (socket) {
+
+
+  // A User starts a path
+  socket.on( 'startPath', function( data, sessionId ) {
+
+    socket.broadcast.emit( 'startPath', data, sessionId );
+
   });
+
+  // A User continues a path
+  socket.on( 'continuePath', function( data, sessionId ) {
+
+    socket.broadcast.emit( 'continuePath', data, sessionId );
+
+  });
+
+  // A user ends a path
+  socket.on( 'endPath', function( data, sessionId ) {
+
+    socket.broadcast.emit( 'endPath', data, sessionId );
+
+  });
+  
 });
-
-
-module.exports = app;
